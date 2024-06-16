b4d60bf667a1eae8e8adf3ba2958620cd0ae729e
Afonso Ferreira <29693141+afonsoni@users.noreply.github.com>
2024-06-16 20:22:03 +0100

    apagado README e retirada biblioteca de Criar.js

--- festas/README.md
+++
@@ -1,7 +0,0 @@
-# Instalação
-
-npm install -D tailwindcss postcss autoprefixer
-npm install react-router-dom react-helmet-async
-
-
-pip install -r requirements.txt
--- festas/src/pages/Criar.js
+++ festas/src/pages/Criar.js
@@ -5,7 +5,6 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { registerLocale } from 'react-datepicker';
 import pt from 'date-fns/locale/pt';
-import { set } from 'mongoose';
 
 registerLocale('pt', pt);
 
